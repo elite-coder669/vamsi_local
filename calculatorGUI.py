d5a2603b97e2d7407798c82295849e90f3a47f1d
elite-coder669 <vamsikrishnamallupeddi22@gmail.com>
2025-01-05 21:00:17 +0530

    final commit v1 calculator

--- calculatorGUI.py
+++ calculatorGUI.py
@@ -18,11 +18,11 @@ def evaluator():
         entry.delete(0,END)
         entry.insert(0,"Invalid")
         # time.sleep(3)
-        entry.insert(0,f"{e}")
+        
 
 def restrict_keys(event):
     # Define the allowed keys for the calculator
-    allowed_keys = "0123456789+-*/.s"  # Include "s" for sqrt
+    allowed_keys = "0123456789+-*/."  # Include "s" for sqrt
     if event.char in allowed_keys:
         return True  # Allow the key press
     else:
@@ -32,14 +32,14 @@ def restrict_keys(event):
 
 # Function to bind keys for specific buttons
 def bind_keys():
-    calculatorApp.bind("<s>", lambda event: entry.insert(INSERT, "√"))  # Bind 's' for sqrt button
+    # calculatorApp.bind("<s>", lambda event: entry.insert(INSERT, "√"))  # Bind 's' for sqrt button
     calculatorApp.bind("<Return>", lambda event: evaluator())  # Bind 'Return' key for equal (=) button
 
 
 
 # Create main application window
 calculatorApp = Tk()
-calculatorApp.geometry("350x450")
+calculatorApp.geometry("210x300")
 calculatorApp.title("Calculator")
 calculatorApp.config(bg="White")
 calculatorApp.resizable(False,False)
@@ -65,65 +65,65 @@ entry.grid(row=0, column=0,columnspan=4, sticky="ew", padx=10, pady=10)
 
 
 # Calculator buttons
-cancel_button = Button(calculatorApp, text="C", fg='red',font=("Arial", 22),command= lambda: entry.delete(0, END))
-cancel_button.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+sqrt = Button(calculatorApp, text="(", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"("))
+sqrt.grid(row=1, column=0, sticky="ew")
 
-back_space = Button(calculatorApp, text="🔙", height=2,command= lambda: entry.delete(len(entry.get())-1, END))
-back_space.grid(row=1, column=1, sticky="ew", padx=5, pady=5)
+percentage = Button(calculatorApp, text=")", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,")"))
+percentage.grid(row=1, column=1, sticky="ew")
 
-percentage = Button(calculatorApp, text="%", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"%"))
-percentage.grid(row=1, column=2, sticky="ew", padx=5, pady=5)
+back_space = Button(calculatorApp, text=" 🔙 ", height=2,command= lambda: entry.delete(len(entry.get())-1, END))
+back_space.grid(row=1, column=2, sticky="ew")
 
-by = Button(calculatorApp, text="/", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT, "/"))
-by.grid(row=1, column=3, sticky="ew", padx=5, pady=5)
+by = Button(calculatorApp, text="/", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT, "/"))
+by.grid(row=1, column=3, sticky="ew")
 
-seven = Button(calculatorApp, text="7", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"7"))
-seven.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+seven = Button(calculatorApp, text="7", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"7"))
+seven.grid(row=2, column=0, sticky="ew")
 
-eight = Button(calculatorApp, text="8", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"8"))
-eight.grid(row=2, column=1, sticky="ew", padx=5, pady=5)
+eight = Button(calculatorApp, text="8", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"8"))
+eight.grid(row=2, column=1, sticky="ew")
 
-nine = Button(calculatorApp, text="9", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"9"))
-nine.grid(row=2, column=2, sticky="ew", padx=5, pady=5)
+nine = Button(calculatorApp, text="9", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"9"))
+nine.grid(row=2, column=2, sticky="ew")
 
-cross = Button(calculatorApp, text="*", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"*"))
-cross.grid(row=2, column=3, sticky="ew", padx=5, pady=5)
+cross = Button(calculatorApp, text="*", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"*"))
+cross.grid(row=2, column=3, sticky="ew")
 
-four = Button(calculatorApp, text="4", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"4"))
-four.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
+four = Button(calculatorApp, text="4", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"4"))
+four.grid(row=3, column=0, sticky="ew")
 
-five = Button(calculatorApp, text="5", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"5"))
-five.grid(row=3, column=1, sticky="ew", padx=5, pady=5)
+five = Button(calculatorApp, text="5", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"5"))
+five.grid(row=3, column=1, sticky="ew")
 
-six = Button(calculatorApp, text="6", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"6"))
-six.grid(row=3, column=2, sticky="ew", padx=5, pady=5)
+six = Button(calculatorApp, text="6", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"6"))
+six.grid(row=3, column=2, sticky="ew")
 
-minus = Button(calculatorApp, text="-", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"-"))
-minus.grid(row=3, column=3, sticky="ew", padx=5, pady=5)
+minus = Button(calculatorApp, text="-", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"-"))
+minus.grid(row=3, column=3, sticky="ew")
 
-one = Button(calculatorApp, text="1", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"1"))
-one.grid(row=4, column=0, sticky="ew", padx=5, pady=5)
+one = Button(calculatorApp, text="1", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"1"))
+one.grid(row=4, column=0, sticky="ew")
 
-two = Button(calculatorApp, text="2", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"2"))
-two.grid(row=4, column=1, sticky="ew", padx=5, pady=5)
+two = Button(calculatorApp, text="2", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"2"))
+two.grid(row=4, column=1, sticky="ew")
 
-three = Button(calculatorApp, text="3", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"3"))
-three.grid(row=4, column=2, sticky="ew", padx=5, pady=5)
+three = Button(calculatorApp, text="3", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"3"))
+three.grid(row=4, column=2, sticky="ew")
 
-plus = Button(calculatorApp, text="+", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"+"))
-plus.grid(row=4, column=3, sticky="ew", padx=5, pady=5)
+plus = Button(calculatorApp, text="+", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"+"))
+plus.grid(row=4, column=3, sticky="ew")
 
-sqrt = Button(calculatorApp, text="√", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"√"))
-sqrt.grid(row=5, column=0, sticky="ew", padx=5, pady=5)
+cancel_button = Button(calculatorApp, text="C", fg='red',font=("Arial", 20),command= lambda: entry.delete(0, END))
+cancel_button.grid(row=5, column=0, sticky="ew")
 
-zero = Button(calculatorApp, text="0", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"0"))
-zero.grid(row=5, column=1, sticky="ew", padx=5, pady=5)
+zero = Button(calculatorApp, text="0", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"0"))
+zero.grid(row=5, column=1, sticky="ew")
 
-dot = Button(calculatorApp, text=".", fg='green', font=("Arial", 22), command= lambda: entry.insert(INSERT,"."))
-dot.grid(row=5, column=2, sticky="ew", padx=5, pady=5)
+dot = Button(calculatorApp, text=".", fg='green', font=("Arial", 20), command= lambda: entry.insert(INSERT,"."))
+dot.grid(row=5, column=2, sticky="ew")
 
-equal = Button(calculatorApp, text="=", fg='green', font=("Arial", 22), command= evaluator)
-equal.grid(row=5, column=3, sticky="ew", padx=5, pady=5)
+equal = Button(calculatorApp, text="=", fg='green', font=("Arial", 20), command= evaluator)
+equal.grid(row=5, column=3, sticky="ew")
 
 bind_keys()
 entry.bind("<Key>", restrict_keys)
